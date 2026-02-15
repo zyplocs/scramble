6c0f55c5e6b030a6948771c61e8a203e28094a52
zyplocs <zyplocked@gmail.com>
2026-02-14 23:52:47 -0600

    Toy with List views

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["C3PO", "Leia", "Luke", "Anakin"]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            Section("Section 1") {
+                Text("Static Row 1")
+                Text("Static Row 2")
+            }
+
+            Section("Section 2") {
+                ForEach(0..<5) {
+                    Text("Dynamic Row \($0)")
+                }
+            }
         }
-        .padding()
+        
+        List(people, id: \.self) {
+            Text($0)
+        }
     }
 }
 
