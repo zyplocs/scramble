b04320032dbaaee87f4aee692f3a90514d1a79e8
zyplocs <zyplocked@gmail.com>
2026-02-15 16:36:11 -0600

    Experiment digit transformations with LaTeX rendering

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -5,28 +5,42 @@
 //  Created by Eli J on 2/13/26.
 //
 
+import SwiftMath
 import SwiftUI
 
 struct ContentView: View {
     let people = ["C3PO", "Leia", "Luke", "Anakin"]
+    let digits: [Double] = [42, 5932, 27, 2580]
 
     var body: some View {
         List {
-            Section("Section 1") {
-                Text("Static Row 1")
-                Text("Static Row 2")
+            Section("Digits") {
+                ForEach(digits, id: \.self) { digit in
+                    Text(digit, format: .number)
+                }
             }
 
-            Section("Section 2") {
-                ForEach(0..<5) {
-                    Text("Dynamic Row \($0)")
+            Section("Square Roots") {
+                ForEach(digits, id: \.self) { digit in
+                    MathView(
+                        latex: #" \sqrt{\#(Int(digit))} "#,
+                        fontSize: 20,
+                        alignment: .left
+                    )
+                    .frame(height: 20)
                 }
             }
-            
-            Section("People") {
-                ForEach(people, id: \.self) { person in
-                    Text(person)
+
+            //            Section("Section 3: People") {
+            //                ForEach(people, id: \.self) { person in
+            //                    Text(person)
+            //                }
+
+            Section("Squares") {
+                ForEach(digits, id: \.self) { digit in
+                    Text(digit * digit, format: .number)
                 }
+
             }
         }
     }
